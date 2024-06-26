3403a06cb11998238ec342d7bfbcee681ebdf334
Royi Kim <hellowol@naver.com>
2024-06-26 23:41:31 +0900

    [섹션7 컬렉션 프레임워크 - HashSet] 직접 구현하는 Set2 - MyHashSetV2

---
+++ src/collection/set/MyHashSetV2.java
@@ -0,0 +1,78 @@
+package collection.set;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class MyHashSetV2 {
+
+    static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    private LinkedList<Object>[] buckets;
+
+    private int size = 0;
+    private int capacity = DEFAULT_INITIAL_CAPACITY;
+
+    public MyHashSetV2() {
+        initBuckets();
+    }
+
+    public MyHashSetV2(int capacity) {
+        this.capacity = capacity;
+        initBuckets();
+    }
+
+    private void initBuckets() {
+        buckets = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    public boolean add(Object value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Object> bucket = buckets[hashIndex];
+        if (bucket.contains(value)) {
+            return false;
+        }
+
+        bucket.add(value);
+        size++;
+        return true;
+    }
+
+    public boolean contains(Object searchValue) {
+        int hashIndex = hashIndex(searchValue); //O(1)
+        LinkedList<Object> bucket = buckets[hashIndex]; //O(1)
+        return bucket.contains(searchValue); //O(n) -> O(1) 충돌로 두개가 있을 수도 있지만 평균적으로 O(1)로 볼 수 있다.
+    }
+
+    public boolean remove(Object value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Object> bucket = buckets[hashIndex];
+        boolean result = bucket.remove(value);
+        if (result) {
+            size--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private int hashIndex(Object value) {
+        // Math.abs(): -1, -10 -> 1, 10 절대값
+        return Math.abs(value.hashCode()) % capacity;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV2{" +
+                "buckets=" + Arrays.toString(buckets) +
+                ", size=" + size +
+                ", capacity=" + capacity +
+                '}';
+    }
+}
